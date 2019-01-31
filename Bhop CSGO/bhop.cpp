b6c37c3434cb65d6fe26b9bacaef7be167221a15
BuildTools <unconfigured@null.spigotmc.org>
2019-01-31 22:20:41 +0100

    functional release 1.0

--- Bhop CSGO/bhop.cpp
+++ Bhop CSGO/bhop.cpp
@@ -3,6 +3,7 @@
 #include <TlHelp32.h>
 #include <thread>
 #include <mmsystem.h>
+#include <thread>
 #include "SlimMem.h"
 
 SlimUtils::SlimMem mem;
@@ -19,11 +20,18 @@ struct Offset {
 	bool m_bDormant = 0xED;
 	int dwForceAttack = 0x30FF378;
 	int TotalHitCount = 0xA388;
+	DWORD m_iHealth = 0x100;
+	DWORD m_iGlowIndex = 0xA3F8;
+	DWORD dwGlowObjectManager = 0x520DA80;
 }O;
 struct Values {
 	DWORD localPlayer;
 	DWORD gameModule;
 	BYTE flag;
+	int LocalTeam;
+	int CrossHairID;
+	int EnemyTeam;
+	DWORD EnemyInCH;
 }V;
 
 
@@ -53,12 +61,12 @@ bool GetPlayer()
 
 	if (V.localPlayer == NULL) {
 		V.localPlayer = mem.Read<DWORD>(V.gameModule + O.dwLocalPlayer);
-		//std::cout << V.localPlayer << std::endl;
 		return false;
 	}
 	return true;
 
 }
+
 void bhop()
 {
 	V.flag = mem.Read<BYTE>(V.localPlayer + O.m_fFlags);
@@ -70,17 +78,24 @@ void bhop()
 
 void trigger()
 {
-	int LocalPlayer_inCross = mem.Read<int>(V.localPlayer + O.m_iCrosshairId);
-	int LocalPlayer_Team = mem.Read<int>(V.localPlayer + O.m_iTeamNum);
+	
+}
 
-	DWORD Trigger_EntityBase = mem.Read<DWORD>(V.gameModule + O.dwEntityList + ((LocalPlayer_inCross - 1) * 0x10));
-	int Trigger_EntityTeam = mem.Read<int>(Trigger_EntityBase + O.m_iTeamNum);
-	bool Trigger_EntityDormant = mem.Read<bool>(Trigger_EntityBase + O.m_bDormant);
-
-	if ((LocalPlayer_inCross > 0 && LocalPlayer_inCross <= 64) && (Trigger_EntityBase != NULL) && (Trigger_EntityTeam != LocalPlayer_Team) && (!Trigger_EntityDormant))
+void hitsound()
+{
+	int hitcount = mem.Read<int>(V.localPlayer + O.TotalHitCount);
+	static int oldhit = hitcount;
+	std::cout << oldhit << std::endl;
+	std::cout << hitcount << std::endl;
+	if (hitcount > oldhit)
 	{
-		Sleep(10);
-		mem.Write<int>(V.gameModule + O.dwForceAttack, 5);
+		oldhit++;
+		PlaySound(("hit.wav"), NULL, SND_ASYNC);
+		std::cout << hitcount << std::endl;
+	}
+	if (oldhit > 1)
+	{
+		oldhit = hitcount;
 	}
 }
 
@@ -92,25 +107,28 @@ int main()
 	}
 	std::cout << "pid: " << pid << std::endl;
 	Sleep(500);
-
 	mem.Open(pid, SlimUtils::ProcessAccess::Full);
-
 	GetMod();
-
-	std::cout << "test";
+	GetPlayer();
 
 	while (true)
 	{
 		GetPlayer();
-		bool hit = false;
+		int LocalPlayer_inCross = mem.Read<int>(V.localPlayer + O.m_iCrosshairId);
+		int LocalPlayer_Team = mem.Read<int>(V.localPlayer + O.m_iTeamNum);
 
-		int hitcount = mem.Read<int>(V.localPlayer + O.TotalHitCount);
-		int oldhit=1;
-		if (hitcount > oldhit)
+		DWORD Trigger_EntityBase = mem.Read<DWORD>(V.gameModule + O.dwEntityList + ((LocalPlayer_inCross - 1) * 0x10));
+		int Trigger_EntityTeam = mem.Read<int>(Trigger_EntityBase + O.m_iTeamNum);
+
+		if ((LocalPlayer_inCross > 0 && LocalPlayer_inCross <= 64) && (Trigger_EntityBase != NULL) && (Trigger_EntityTeam != LocalPlayer_Team))
 		{
-			PlaySound(("hit.wav"), NULL, SND_SYNC);
-			std::cout << hitcount << std::endl;
+			mem.Write<int>(V.gameModule + O.dwForceAttack, 5);
+			Sleep(100);
+			mem.Write<int>(V.gameModule + O.dwForceAttack, 4);
+			Sleep(45);
 		}
+		bhop();
+		hitsound();
 	}
 
 	return 0;
